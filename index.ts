3ac282646a09615a1afa61985b49da2df579166e
pgsill <pgsill>
2022-08-21 18:20:53 -0300

    Reply only to own messages

--- index.ts
+++ index.ts
@@ -1,48 +1,53 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf } from 'telegraf';
 import {
   replyHandler,
   photoMessageHandler,
   entityMessageHandler,
   directMentionHandler,
-} from './src/handlers/index'
+} from './src/handlers/index';
 
-import config from './config.json'
+import config from './config.json';
 
 // Quit on missing env var
 if (!process.env?.TELEGRAM_TOKEN) {
-  console.error('Failed to get TELEGRAM_TOKEN from env.')
-  process.exit(1)
+  console.error('Failed to get TELEGRAM_TOKEN from env.');
+  process.exit(1);
 }
 // Quit on missing messages json
 if (!config) {
-  console.error('Failed to get config.json in root folder')
-  process.exit(1)
+  console.error('Failed to get config.json in root folder');
+  process.exit(1);
 }
 
 // Instantiate bot
-const bot = new Telegraf(process.env?.TELEGRAM_TOKEN)
+const bot = new Telegraf(process.env?.TELEGRAM_TOKEN);
 
 // Set up handlers
 bot.on('text', (ctx) => {
-  console.log(ctx.message.entities)
+  console.log(ctx.message.entities);
 
   if (ctx?.update?.message?.reply_to_message) {
-    return replyHandler(ctx)
+    const replyUser = ctx?.update?.message?.reply_to_message?.from?.id;
+    const botId = bot.botInfo?.id;
+
+    if (replyUser === botId) {
+      return replyHandler(ctx);
+    }
   }
 
-  entityMessageHandler(ctx)
-  directMentionHandler(ctx, bot)
-})
+  entityMessageHandler(ctx);
+  directMentionHandler(ctx, bot);
+});
 
 bot.on('photo', (ctx) => {
-  console.log(ctx.message)
+  console.log(ctx.message);
 
-  photoMessageHandler(ctx, bot)
-})
+  photoMessageHandler(ctx, bot);
+});
 
-bot.launch()
-console.log('SIPCHAN ONLINE.')
+bot.launch();
+console.log('SIPCHAN ONLINE.');
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
